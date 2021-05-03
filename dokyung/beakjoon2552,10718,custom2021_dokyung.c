3e82a62291391a0ebe270b8336a490ad5a73f383
danaKim-dokyung <83184270+danaKim-dokyung@users.noreply.github.com>
2021-05-03 16:27:42 +0900

    Add files via upload

---
+++ dokyung/beakjoon2552,10718,custom2021_dokyung.c
@@ -0,0 +1,37 @@
+//
+//  main.c
+//  Beakjoon_2557_dokyung
+//
+//  Created by 도경 on 2021/05/03.
+//
+
+//beakjoon 2557번
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World!");
+    return 0;
+}
+
+//beakjoon 10718번
+#include <stdio.h>
+
+int main(void)
+{
+    printf("강한친구 대한육군\n");
+    printf("강한친구 대한육군");
+    
+    return 0;
+}
+
+
+//Codecure 2021 지원서류 문제 4번
+#include <stdio.h>
+
+int main(void)
+{
+    printf("dokyung kim\n global financial management");
+    
+}
+
