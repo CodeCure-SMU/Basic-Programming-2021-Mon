3b77d6c4bc539a9be587cb7e6e1f626df8e9acb9
wkdtnqls0506 <83440978+wkdtnqls0506@users.noreply.github.com>
2021-05-10 01:04:41 +0900

    Add week 2 homeworks

---
+++ Baekjoon-10869-장수빈.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+
+	scanf_s("%d %d", &A, &B);
+
+	printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	printf("%d\n", A * B);
+	printf("%d\n", A / B);
+	printf("%d\n", A % B);
+
+	return 0;
+
+}
---
+++ Baekjoon-1330-장수빈.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+
+	scanf_s("%d %d", &A, &B);
+
+	if (A > B)
+		printf(">");
+
+	else if (A < B)
+		printf("<");
+
+	else
+		printf("==");
+
+	return 0;
+}
