c61cef9922b1cf864f2a0b111f61d3341db16dc4
lumosmaxima02 <82692225+lumosmaxima02@users.noreply.github.com>
2021-05-10 18:15:55 +0900

    add 2 week homeworks
    add 2 week homeworks

---
+++ Baekjoon_1330_희진.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int num1, num2;
+	scanf("%d %d", &num1, &num2);
+	if (num1 > num2)
+		printf(">");
+	else if
+		(num1 == num2)
+		printf("==");
+	else
+		printf("<");
+	return 0;
