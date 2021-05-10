33743fb74168250d787d851383f542809c58ef96
kim75528028 <82487063+kim75528028@users.noreply.github.com>
2021-05-10 19:47:56 +0900

    Add files via upload

---
+++ TaeHun/BaeKJoon_1330_김태훈.cpp
@@ -0,0 +1,17 @@
+﻿#include "pch.h"
+#include <stdio.h>
+
+int main()
+{
+	int A, B;
+	printf("정수 A를 입력해주세요.");
+	scanf_s("%d", &A);
+	printf("정수 B를 입력해주세요.");
+	scanf_s("%d", &B);
+
+	printf("%d+%d" & A, &B);
+	printf("%d-%d" & A, &B);
+	printf("%d*%d" & A, &B);
+	printf("%d/%d" & A, &B);
+	printf("%d%%d" & A, &B);
+}
---
+++ TaeHun/BaekJoon_2753_김태훈.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+#include <stdio.h>
+
+
+int main()
+{
+	int year=0;
+	printf("연도를 입력하세요.");
+	scanf_s("%d", year);
+
+	if ((year % 4 == 0) & (year % 100 == 0) || year % 400 == 0)
+		printf("1");
+	else
+		printf("0");
+}
