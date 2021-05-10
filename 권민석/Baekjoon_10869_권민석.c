b75a6c02f4557e2ca55840442feee6f433dc598f
kms1064 <82499690+kms1064@users.noreply.github.com>
2021-05-10 19:27:48 +0900

    Add files via upload

---
+++ 권민석/Baekjoon_10869_권민석.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+	scanf_s("%d %d", &A, &B);
+	1 <= A;
+	A <= 10000;
+	1 <= B;
+	B <= 10000;
+	printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	printf("%d\n", A * B);
+	printf("%d\n", A / B);
+	printf("%d\n", A % B);
+
+	return 0;
+}
