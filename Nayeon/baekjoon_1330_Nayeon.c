8a876c012b639f16f1f3e07291cb7a500a17bb21
opew1038 <83111019+opew1038@users.noreply.github.com>
2021-05-16 01:53:39 +0900

    2주차 과제

---
+++ Nayeon/baekjoon_1330_Nayeon.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+
+    if (A > B)
+    {
+       printf(">");
+    }
+    else if(A < B)
+    {
+        printf("<");
+    }
+    else if(A == B)
+    {
+        printf("==");
+    }
+
+    return 0;
+}
