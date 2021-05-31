6a5c40a94d6c569155db07ddf1b9e032b83f555d
kim75528028 <82487063+kim75528028@users.noreply.github.com>
2021-05-31 19:26:28 +0900

    Add files via upload

---
+++ TaeHun/Backjoon_2562_김태훈.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <stdio.h>
+
+int main()
+{
+	int num[9], count = 0, max, i;
+
+	for (i = 1; i < 10; i++)
+	{
+		scanf_s("%d", &num[i]);
+	}
+	max = num[0];
+
+	for (i = 1; i < 10; i++)
+	{
+		if (num[i + 1] > max)
+		{
+			count = i + 1;
+			max = num[i + 1];
+		}
+	}
+	printf("%d\n%d", max, count);
+
+	return 0;
+
+}
